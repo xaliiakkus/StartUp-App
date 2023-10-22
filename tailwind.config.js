8ad7a2fd6c3676239a01e447d77dfc9a63763163
xaliiakkus <xaliakkus@gmail.com>
2023-10-22 15:08:52 +0300

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,6 @@ module.exports = {
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
   ],
   theme: {
     extend: {},
